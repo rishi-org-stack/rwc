57ffcb1fe82c7be4799f89e10db6d7ea53a0c19d
rishi_muni <rishi.jha_xt@angkas.com>
2024-01-06 20:24:56 +0530

    feat: only 4 threads to count words

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,42 @@ fn main() {
 
     let file_names: Vec<String> = command[1..].to_vec();
 
+    let iterations: usize = match (command_size - 1) % 4 {
+        0 => (command_size - 1) / 4,
+        _ => ((command_size - 1) / 4) + 1,
+    };
+
     let (tx, rx) = mpsc::channel();
     file_names.iter().for_each(|file_name| {
-        tx.send(file_name.clone()).expect("failed to send message");
+        tx.send(fs::File::open(file_name.as_str()).unwrap())
+            .expect("failed to send message");
     });
 
-    let threads = (0..(command_size - 1))
-        .into_iter()
-        .fold(vec![], |mut acc, _| {
-            let file_name = rx.recv().unwrap();
-            let t = thread::spawn(move || {
-                let mut file = fs::File::open(file_name.as_str()).unwrap();
-                let mut buf = String::new();
-                file.read_to_string(&mut buf).unwrap();
-                println!("chars: {}", buf.chars().count())
-            });
+    let mut i = command_size - 1;
+    (0..iterations).into_iter().for_each(|_| {
+        let thread_iteration = match i {
+            x if x >= 4 => 4,
+            _ => i,
+        };
+        let threads: Vec<thread::JoinHandle<()>> =
+            (0..thread_iteration)
+                .into_iter()
+                .fold(vec![], |mut acc, _| {
+                    let mut file = rx.recv().unwrap();
 
-            acc.push(t);
-            acc
-        });
+                    let t = thread::spawn(move || {
+                        let mut buf = String::new();
+                        file.read_to_string(&mut buf).unwrap();
+                        println!("chars: {}", buf.chars().count());
+                    });
 
-    for t in threads {
-        t.join().unwrap();
-    }
+                    i -= 1;
+                    acc.push(t);
+                    acc
+                });
+
+        for t in threads {
+            t.join().unwrap();
+        }
+    });
 }
